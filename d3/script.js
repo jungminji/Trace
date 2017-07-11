856917194a42ae394dfe24d266467cf6b29e58ec
jungminji <fdev.jm@gmail.com>
2017-07-11 21:51:24 +0900

    Pug file modualized, d3 basic chart

--- d3/script.js
+++ d3/script.js
@@ -16,20 +16,81 @@ var numArray = [12, 20, 33, 40, 55, 60, 75, 100, 120, 130];
 //         return `${data}px`;
 //     })
 
-let li = d3.select('.items').selectAll('li')
-            .data(numArray)
-            .text(function(data){
-                return `This is item number ${data}`;
-            })
 
-            li.enter().append('li')
-            .text(function(data){
-                return `This is item number ${data}`;
-            })
+// Append more list items
 
-            li.exit().remove();
+// let li = d3.select('.items').selectAll('li')
+//             .data(numArray)
+//             .text(function(data){
+//                 return `This is item number ${data}`;
+//             })
+
+//             li.enter().append('li')
+//             .text(function(data){
+//                 return `This is item number ${data}`;
+//             })
+
+//             li.exit().remove();
 
 
-let name = 'Jungmin Ji';
+// let name = 'Jungmin Ji';
 
-d3.select('.name').text(name);
+// d3.select('.name').text(name);
+
+
+// Basic Static Chart
+let styles = [{
+        'background': 'red',
+        'color': 'while',
+        'width': '35',
+    },
+    {
+        'background': 'green',
+        'color': 'while',
+        'width': '70',
+    },
+    {
+        'background': 'blue',
+        'color': 'while',
+        'width': '45',
+    },
+    {
+        'background': 'tan',
+        'color': 'while',
+        'width': '10',
+    },
+    {
+        'background': 'skyblue',
+        'color': 'while',
+        'width': '80',
+    },
+    {
+        'background': 'pink',
+        'color': 'while',
+        'width': '50',
+    },
+    {
+        'background': 'orange',
+        'color': 'while',
+        'width': '20',
+    }
+];
+
+
+d3.selectAll('.items li')
+    .data(styles)
+    .style({
+        'font-size': '18px',
+        'padding': '6px',
+        'margin': '4px',
+        'list-style': 'none',
+        'background': function (d) {
+            return d.background;
+        },
+        'color': function (d) {
+            return d.color;
+        },
+        'width': function (d) {
+            return `${d.width}%`;
+        }
+    })
